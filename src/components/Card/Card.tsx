915049c25a9cb799c97765906a66f0d6ec01c302
HaynrikhMilies <haynrikh.santrosyan@milies.net>
2024-05-30 19:38:23 +0400

    Card Placeholder

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,16 +4,41 @@ interface CardProps {
   title: string;
   description: string;
   price: number;
+  isLoading: boolean;
 }
 
-const Card = ({ imageUrl, title, description, price }: CardProps) => {
+const Card = ({ imageUrl, title, description, price, isLoading }: CardProps) => {
+  if (isLoading) {
+    return (
+      <div className="card" aria-hidden="true" style={{ width: "18rem" }}>
+        <img src="..." className="card-img-top" alt="Loading..." />
+        <div className="card-body">
+          <h5 className="card-title placeholder-glow">
+            <span className="placeholder col-6"></span>
+          </h5>
+          <p className="card-text placeholder-glow">
+            <span className="placeholder col-7"></span>
+            <span className="placeholder col-4"></span>
+            <span className="placeholder col-4"></span>
+            <span className="placeholder col-6"></span>
+            <span className="placeholder col-8"></span>
+          </p>
+          <a
+            className="btn btn-primary disabled placeholder col-3"
+            aria-disabled="true"
+          ></a>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="card" style={{ width: "18rem" }}>
       <img src={imageUrl} className="card-img-top" alt={title} />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
-        <p className="card-price">${price.toFixed(2)}</p>
+        <p className="card-price">${price?.toFixed(2)}</p>
         <a href="#" className="btn btn-primary">
           Buy
         </a>
--- src/components/ShopPage/ShopPage.tsx
+++ src/components/ShopPage/ShopPage.tsx
@@ -1,88 +1,174 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
-
+interface dataProps {
+  imageUrl: string;
+  title: string;
+  description: string;
+  price: number;
+}
 const ShopPage = () => {
-  const cardData = [
+  const [data, setData] = useState<dataProps[]>([
     {
-      imageUrl: "HeartCandle1.png",
-      title: "Card 1",
-      description: "Description 1",
-      price: 10,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "HeartCandle2.png",
-      title: "Card 2",
-      description: "Description 2",
-      price: 20,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "SquareCircleAndZigzagCandle.png",
-      title: "Card 3",
-      description: "Description 3",
-      price: 30,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "SquareCircleCandle1.png",
-      title: "Card 4",
-      description: "Description 4",
-      price: 40,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "SquareCircleCandle2.png",
-      title: "Card 5",
-      description: "Description 5",
-      price: 50,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "SquareCircleCandle2.png",
-      title: "Card 6",
-      description: "Description 6",
-      price: 60,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "HeartCandle1.png",
-      title: "Card 7",
-      description: "Description 7",
-      price: 70,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "HeartCandle2.png",
-      title: "Card 8",
-      description: "Description 8",
-      price: 80,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "SquareCircleAndZigzagCandle.png",
-      title: "Card 9",
-      description: "Description 9",
-      price: 90,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "HeartCandle1.png",
-      title: "Card 10",
-      description: "Description 10",
-      price: 100,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "SquareCircleCandle2.png",
-      title: "Card 11",
-      description: "Description 11",
-      price: 110,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
     {
-      imageUrl: "SquareCircleCandle1.png",
-      title: "Card 12",
-      description: "Description 12",
-      price: 120,
+      imageUrl: "",
+      title: "",
+      description: "",
+      price: 0,
     },
-  ];
+  ]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData([
+        {
+          imageUrl: "HeartCandle1.png",
+          title: "Card 1",
+          description: "Description 1",
+          price: 10,
+        },
+        {
+          imageUrl: "HeartCandle2.png",
+          title: "Card 2",
+          description: "Description 2",
+          price: 20,
+        },
+        {
+          imageUrl: "SquareCircleAndZigzagCandle.png",
+          title: "Card 3",
+          description: "Description 3",
+          price: 30,
+        },
+        {
+          imageUrl: "SquareCircleCandle1.png",
+          title: "Card 4",
+          description: "Description 4",
+          price: 40,
+        },
+        {
+          imageUrl: "SquareCircleCandle2.png",
+          title: "Card 5",
+          description: "Description 5",
+          price: 50,
+        },
+        {
+          imageUrl: "SquareCircleCandle2.png",
+          title: "Card 6",
+          description: "Description 6",
+          price: 60,
+        },
+        {
+          imageUrl: "HeartCandle1.png",
+          title: "Card 7",
+          description: "Description 7",
+          price: 70,
+        },
+        {
+          imageUrl: "HeartCandle2.png",
+          title: "Card 8",
+          description: "Description 8",
+          price: 80,
+        },
+        {
+          imageUrl: "SquareCircleAndZigzagCandle.png",
+          title: "Card 9",
+          description: "Description 9",
+          price: 90,
+        },
+        {
+          imageUrl: "HeartCandle1.png",
+          title: "Card 10",
+          description: "Description 10",
+          price: 100,
+        },
+        {
+          imageUrl: "SquareCircleCandle2.png",
+          title: "Card 11",
+          description: "Description 11",
+          price: 110,
+        },
+        {
+          imageUrl: "SquareCircleCandle1.png",
+          title: "Card 12",
+          description: "Description 12",
+          price: 120,
+        },
+      ]);
+      setLoading(false);
+    }, 1000);
+  }, []);
 
   return (
     <div className="container text-center">
       <div className="row g-2">
-        {cardData.map((card, index) => (
+        {data.map((card, index) => (
           <div key={index} className="col-md-3 col-sm-6 mb-4">
-            <Card {...card} />
+            <Card {...card} isLoading={loading} />
           </div>
         ))}
       </div>
